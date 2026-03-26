2d1ff7140e77aeb7e9ae03d2bf62672edd9b09bf
juanda <juanda@users.noreply.github.com>
2026-03-26 08:49:34 +0000

    Remove preview for PR 21 🛬

--- pr-preview/pr-21/_astro/Base.astro_astro_type_script_index_0_lang.CbE1S-Az.js
+++
@@ -1,1 +0,0 @@
-import{g as n}from"./get-api-client.D04N02NI.js";import"./preload-helper.D1qPEhkD.js";const s=document.getElementById("nav-login"),e=document.getElementById("nav-user");(async()=>{try{const t=await(await n()).getSession();t?.user&&(s.style.display="none",e.textContent=t.user.email,e.style.display="")}catch{}})();
