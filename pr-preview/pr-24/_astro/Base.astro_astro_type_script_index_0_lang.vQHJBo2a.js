c7e70b0032624b70622f8e68c77cd9e842d3da80
juanda <juanda@users.noreply.github.com>
2026-03-26 09:29:21 +0000

    Remove preview for PR 24 🛬

--- pr-preview/pr-24/_astro/Base.astro_astro_type_script_index_0_lang.vQHJBo2a.js
+++
@@ -1,1 +0,0 @@
-import{g as n}from"./get-api-client.D2Cz4XkI.js";import"./preload-helper.Lv3cVQ5s.js";const s=document.getElementById("nav-login"),e=document.getElementById("nav-user");(async()=>{try{const t=await(await n()).getSession();t?.user&&(s.style.display="none",e.textContent=t.user.email,e.style.display="")}catch{}})();
