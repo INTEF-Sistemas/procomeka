78e2c68d93f6df7f1d3e3c86193ef8a5e0e2ec01
erseco <erseco@users.noreply.github.com>
2026-03-26 18:51:46 +0000

    Deploy preview for PR 42 🛫

---
+++ pr-preview/pr-42/_astro/Base.astro_astro_type_script_index_0_lang.I2nU8tOi.js
@@ -0,0 +1,1 @@
+import{g as n}from"./get-api-client.SPDlglll.js";import"./preload-helper.GQMbRmRW.js";const s=document.getElementById("nav-login"),e=document.getElementById("nav-user");(async()=>{try{const t=await(await n()).getSession();t?.user&&(s.style.display="none",e.textContent=t.user.email,e.style.display="")}catch{}})();
