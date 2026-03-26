a13ed6d7e472488ce00647433912338b6ab67b56
juanda <juanda@users.noreply.github.com>
2026-03-26 17:46:40 +0000

    Deploy preview for PR 38 🛫

---
+++ pr-preview/pr-38/_astro/Base.astro_astro_type_script_index_0_lang.BYTrTKJU.js
@@ -0,0 +1,1 @@
+import{g as n}from"./get-api-client.-5JqrhP8.js";import"./preload-helper.kbPugHWE.js";const s=document.getElementById("nav-login"),e=document.getElementById("nav-user");(async()=>{try{const t=await(await n()).getSession();t?.user&&(s.style.display="none",e.textContent=t.user.email,e.style.display="")}catch{}})();
