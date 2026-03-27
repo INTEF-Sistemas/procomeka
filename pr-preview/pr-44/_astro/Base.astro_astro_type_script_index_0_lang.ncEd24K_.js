0d437994a8b190d08607988f6e5daa134f41742c
juanda <juanda@users.noreply.github.com>
2026-03-27 10:35:24 +0000

    Deploy preview for PR 44 🛫

---
+++ pr-preview/pr-44/_astro/Base.astro_astro_type_script_index_0_lang.ncEd24K_.js
@@ -0,0 +1,1 @@
+import{g as n}from"./get-api-client.C4gkuR88.js";import"./preload-helper.DQ6R_Zku.js";const s=document.getElementById("nav-login"),e=document.getElementById("nav-user");(async()=>{try{const t=await(await n()).getSession();t?.user&&(s.style.display="none",e.textContent=t.user.email,e.style.display="")}catch{}})();
